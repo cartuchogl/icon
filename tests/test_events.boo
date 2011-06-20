874af43060267136af75d3c9cb6123c0226ab6ba
Carlos Urbín <carlos@urbin.es>
2011-06-20 21:53:52 +0100

    Remaking tests.

--- tests/test_events.boo
+++ tests/test_events.boo
@@ -1,43 +1,24 @@
 namespace GameEngine
 
 import System
+import Horde3DNET
+import Horde3DNET.Utils
 
 Platform.Methods.setWindow("test events",640,480,0)
 
-CoreEvents.onpostinit += def(obj):
-  Console.WriteLine(Horde3DNET.H3d.getVersionString())
+H3d.init()
 
-CoreEvents.onkeydown += def(obj,args):
-  s = "onkeydown -> state:{0}, scancode:{1}, unicode:{2}, sym:{3}, mod:{4}"
-  print s % (args['state'], args['scancode'], args['unicode'], args['sym'], args['mod'])
+Console.WriteLine(H3d.getVersionString())
+Console.WriteLine(Platform.Methods.getPlatform())
+Console.WriteLine(Platform.Methods.getCpuFlags())
 
-CoreEvents.onkeyup += def(obj,args):
-  s = "onkeyup -> state:{0}, scancode:{1}, unicode:{2}, sym:{3}, mod:{4}"
-  print s % (args['state'], args['scancode'], args['unicode'], args['sym'], args['mod'])
+running = true
+while running:
+  while evt = Platform.Methods.checkEvents():
+    running = false if evt == 'quit'
+    Console.WriteLine(evt) if evt
 
-CoreEvents.onmousedown += def(obj,args):
-  s = "onmousedown -> x:{0}, y:{1}, state: {2}, button: {3}"
-  print s % (args['x'], args['y'], args['state'], args['button'])
+Horde3DUtils.dumpMessages()
 
-CoreEvents.onmouseup += def(obj,args):
-  s = "onmouseup -> x:{0}, y:{1}, state: {2}, button: {3}"
-  print s % (args['x'], args['y'], args['state'], args['button'])
-
-CoreEvents.onmousemove += def(obj,args):
-  s = "onmousemove -> x:{0}, y:{1}, xrel:{2}, yrel:{3}"
-  print s % (args['x'],args['y'],args['xrel'],args['yrel'])
-  
-acum = 0
-frames = 0
-
-CoreEvents.onframe += def(obj):
-  acum += 1
-  frames += 1
-  print "frame ${frames}" if acum>1000
-
-CoreEvents.onendframe += def(obj):
-  acum = 0 if acum>1000
-
-CoreEvents.onend += def(obj):
-  print "onend"
+H3d.release()
 
--- tests/test_events.cs
+++ tests/test_events.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections;
+using Horde3DNET;
+using Horde3DNET.Utils;
 
 namespace GameEngine {
   public class TestEvents {
@@ -9,63 +11,28 @@ namespace GameEngine {
     public static int Main(string[] args) {
       Platform.Methods.setWindow("test events",640,480,0);
       
-      CoreEvents.onpostinit += new CoreEvent(delegate(object caller) {
-        Console.WriteLine(Horde3DNET.H3d.getVersionString());
-      });
-      
-      CoreEvents.onkeydown += new CoreEventData(delegate(object caller, Hashtable arg) {
-        Console.WriteLine(
-          "onkeydown -> state:{0}, scancode:{1}, unicode:{2}, sym:{3}, mod:{4}",
-          arg["state"], arg["scancode"], arg["unicode"], arg["sym"], arg["mod"]
-        );
-      });
-      
-      CoreEvents.onkeyup += new CoreEventData(delegate(object caller, Hashtable arg) {
-        Console.WriteLine(
-          "onkeyup -> state:{0}, scancode:{1}, unicode:{2}, sym:{3}, mod:{4}",
-          arg["state"], arg["scancode"], arg["unicode"], arg["sym"], arg["mod"]
-        );
-      });
+      H3d.init();
 
-      CoreEvents.onmousedown += new CoreEventData(delegate(object caller, Hashtable arg) {
-        Console.WriteLine(
-          "onmousedown -> x:{0}, y:{1}, state: {2}, button: {3}",
-          arg["x"], arg["y"], arg["state"], arg["button"]
-        );
-      });
+      Console.WriteLine(H3d.getVersionString());
+      Console.WriteLine(Platform.Methods.getPlatform());
+      Console.WriteLine(Platform.Methods.getCpuFlags());
 
-      CoreEvents.onmouseup += new CoreEventData(delegate(object caller, Hashtable arg) {
-        Console.WriteLine(
-          "onmouseup -> x:{0}, y:{1}, state: {2}, button: {3}",
-          arg["x"], arg["y"], arg["state"], arg["button"]
-        );
-      });
-
-      CoreEvents.onmousemove += new CoreEventData(delegate(object caller, Hashtable arg) {
-        Console.WriteLine(
-          "onmousemove -> x:{0}, y:{1}, xrel:{2}, yrel:{3}",
-          arg["x"], arg["y"], arg["xrel"], arg["yrel"]
-        );
-      });
-      
-      CoreEvents.onframe += new CoreEvent(delegate(object caller) {
-        acum += 1;
-        frames += 1;
-        if( acum>1000)
-          Console.WriteLine( "frame " + frames );
-      });
-
-      CoreEvents.onendframe += new CoreEvent(delegate(object caller) {
-        if( acum>1000 ) acum = 0;
-      });
-
-      CoreEvents.onend += new CoreEvent(delegate(object caller) {
-        Console.WriteLine( "onend" );
-      });
-      
-      for(int c=0;c<100000;c++) {
-        Console.WriteLine(c);
+      bool running = true;
+      while(running) {
+        string evt;
+        while((evt = Platform.Methods.checkEvents())!=null) {
+          if(evt == "quit") {
+            running = false;
+          }
+          if(evt!=null) {
+            Console.WriteLine(evt);
+          }
+        }
       }
+
+      Horde3DUtils.dumpMessages();
+
+      H3d.release();
       
       return 0;
     }
